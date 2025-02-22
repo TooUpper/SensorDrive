5239635df34e91619a33933cc612ab3f5310b437
kay.wang <kay@tooupper.com>
2025-02-22 20:49:46 +0800

    Add STM32F103 ADC Template

--- STM32/STM32F103x/裸机/F103_ADC/Drivers/adc1/adc1.c
+++ STM32/STM32F103x/裸机/F103_ADC/Drivers/adc1/adc1.c
@@ -3,43 +3,43 @@
 
 void ADC1_GPIO_Config(void) {
     // 启用GPIOA时钟
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(ADC_GPIO_CLK, ENABLE);
     
     GPIO_InitTypeDef GPIO_InitStructure;
     // 配置PA0为模拟输入模式（ADC1_IN0）
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;    // 模拟输入模式
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = ADC_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Mode = ADC_GPIO_MODE;    // 模拟输入模式
+    GPIO_Init(ADC_GPIO_PORT, &GPIO_InitStructure);
 }
 
 void ADC1_Config(void) {
     // 启用ADC1时钟
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+    RCC_APB2PeriphClockCmd(ADC_PERIPH_CLK, ENABLE);
     
     // 配置ADC时钟分频（PCLK2为72MHz时，分频6得到12MHz）
-    RCC_ADCCLKConfig(RCC_PCLK2_Div6);
+    RCC_ADCCLKConfig(ADC_CLK_DIV);
     
     ADC_InitTypeDef ADC_InitStructure;
     // ADC基础配置
-    ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;        // 独立模式
-    ADC_InitStructure.ADC_ScanConvMode = DISABLE;             // 单通道，禁用扫描模式
-    ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;       // 单次转换模式
-    ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None; // 软件触发
-    ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;    // 数据右对齐
-    ADC_InitStructure.ADC_NbrOfChannel = 1;                   // 转换通道数为1
-    ADC_Init(ADC1, &ADC_InitStructure);
+    ADC_InitStructure.ADC_Mode = ADC_MODE;        // 独立模式
+    ADC_InitStructure.ADC_ScanConvMode = ADC_SCAN_MODE;             // 单通道，禁用扫描模式
+    ADC_InitStructure.ADC_ContinuousConvMode = ADC_CONTINUOUS_MODE;       // 单次转换模式
+    ADC_InitStructure.ADC_ExternalTrigConv = ADC_TRIGGER; // 软件触发
+    ADC_InitStructure.ADC_DataAlign = ADC_DATA_ALIGN;    // 数据右对齐
+    ADC_InitStructure.ADC_NbrOfChannel = ADC_NBR_OF_CHANNELS;                   // 转换通道数为1
+    ADC_Init(ADCx, &ADC_InitStructure);
     
     // 配置通道0（IN0）的采样时间（239.5周期，适合高阻抗信号）
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_239Cycles5);
+    ADC_RegularChannelConfig(ADCx, ADC_CHANNEL, 1, ADC_SAMPLETIME);
     
     // 启用ADC
-    ADC_Cmd(ADC1, ENABLE);
+    ADC_Cmd(ADCx, ENABLE);
     
     // ADC校准流程
-    ADC_ResetCalibration(ADC1);               // 复位校准寄存器
-    while(ADC_GetResetCalibrationStatus(ADC1)); // 等待复位完成
-    ADC_StartCalibration(ADC1);               // 开始校准
-    while(ADC_GetCalibrationStatus(ADC1));    // 等待校准完成
+    ADC_ResetCalibration(ADCx);               // 复位校准寄存器
+    while(ADC_GetResetCalibrationStatus(ADCx)); // 等待复位完成
+    ADC_StartCalibration(ADCx);               // 开始校准
+    while(ADC_GetCalibrationStatus(ADCx));    // 等待校准完成
 }
 
 
@@ -54,9 +54,9 @@ void ADC1_Config(void) {
 
 */
 uint16_t ADC_GetValue(void) {
-    ADC_SoftwareStartConvCmd(ADC1, ENABLE);      // 启动转换
-    while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)); // 等待转换完成
-    return ADC_GetConversionValue(ADC1);         // 返回转换结果
+    ADC_SoftwareStartConvCmd(ADCx, ENABLE);      // 启动转换
+    while(!ADC_GetFlagStatus(ADCx, ADC_FLAG_EOC)); // 等待转换完成
+    return ADC_GetConversionValue(ADCx);         // 返回转换结果
 }
 
 void ADC1_Init(void){
--- STM32/STM32F103x/裸机/F103_ADC/Drivers/adc1/adc1.h
+++ STM32/STM32F103x/裸机/F103_ADC/Drivers/adc1/adc1.h
@@ -5,10 +5,23 @@
 
 /**/
 // GPIO
+#define ADC_GPIO_PORT  GPIOA           // 选择GPIO端口A
+#define ADC_GPIO_PIN   GPIO_Pin_0       // 选择引脚PA0（ADC1_IN0）
+#define ADC_GPIO_CLK   RCC_APB2Periph_GPIOA  // GPIOA时钟使能
+#define ADC_GPIO_MODE  GPIO_Mode_AIN    // 模拟输入模式
 
 // ADC1
-
-
+#define ADCx 				ADC1
+#define ADC_PERIPH_CLK      RCC_APB2Periph_ADC1      // ADC1时钟使能
+#define ADC_CLK_DIV         RCC_PCLK2_Div6           // ADC时钟分频
+#define ADC_MODE            ADC_Mode_Independent     // 独立模式
+#define ADC_SCAN_MODE       DISABLE                  // 单通道模式
+#define ADC_CONTINUOUS_MODE DISABLE                  // 单次转换模式
+#define ADC_TRIGGER         ADC_ExternalTrigConv_None// 软件触发
+#define ADC_DATA_ALIGN      ADC_DataAlign_Right      // 右对齐
+#define ADC_CHANNEL         ADC_Channel_0            // ADC1_CHANNEL_0（对应PA0）
+#define ADC_SAMPLETIME      ADC_SampleTime_239Cycles5 // 239.5周期适合高阻抗信号
+#define ADC_NBR_OF_CHANNELS 1                        // 转换通道数为1
 
 /**/
 void ADC1_Init(void);
